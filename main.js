3e7769245f176534c15fa322f1a1b9823f09173a
Lindsey Maddox <dev@lindseymaddox.com>
2018-10-23 07:53:07 -0500

    not sure because I coded myself to sleep

---
+++ main.js
@@ -0,0 +1,23 @@
+
+// Functions are reusable code. JavaScript written without functions executes one line of code at a time until complete.
+
+// Display the first and last name of the following people
+let firstName = "Jamal";
+let lastName = "Haynes";
+
+console.log(`${firstName} ${lastName}`);
+
+let firstName = "Debra";
+let lastName = "Gordon";
+
+console.log(`${firstName} ${lastName}`);
+
+let firstName = "Svetlana";
+let lastName = "Irinov";
+
+console.log(`${firstName} ${lastName}`);
+
+let firstName = "Sequina";
+let lastName = "Rodriguez";
+
+console.log(`${firstName} ${lastName}`);
