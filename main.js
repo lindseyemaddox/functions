cee36012fc8bccafdbe66f853bec67c4dc316dd4
Lindsey Maddox <dev@lindseymaddox.com>
2018-10-23 08:47:04 -0500

    added some relevant examples to the js file

--- main.js
+++ main.js
@@ -1,23 +1,39 @@
 
 // Functions are reusable code. JavaScript written without functions executes one line of code at a time until complete.
 
-// Display the first and last name of the following people
 let firstName = "Jamal";
 let lastName = "Haynes";
+// console.log(`${firstName} ${lastName}`);
 
-console.log(`${firstName} ${lastName}`);
+function displayName (firstName, lastName) {
+  // console.log(`${firstName} ${lastName}`)
+}
 
-let firstName = "Debra";
-let lastName = "Gordon";
+displayName("Jamal", "Haynes")
+displayName("Debra", "Gordon")
+displayName("Svetlana", "Irinov")
+displayName("Sequina", "Rodriguez")
+displayName("Jessawynne", "Parker")
 
-console.log(`${firstName} ${lastName}`);
 
-let firstName = "Svetlana";
-let lastName = "Irinov";
+// A pure function is one that is consistent and has no effect on anything else in your system.
+// That function will return the exact same value, given the same inputs every time.
+const bugSquasher = function (bug, squasher) {
+  return `The ${bug} was squashed by a ${squasher}`
+}
 
-console.log(`${firstName} ${lastName}`);
+bugSquasher("Beetle", "Boot")
+bugSquasher("Spider", "thing next to my hand when I saw it")
 
-let firstName = "Sequina";
-let lastName = "Rodriguez";
 
-console.log(`${firstName} ${lastName}`);
+// Impure functions produce different results upon execution.
+const noise = function () {
+  return Math.random()
+}
+
+
+// or affect other parts of the system when executed.
+const updateDOM = function (message) {
+    const el = document.getElementById("messages")
+    el.innerHTML += message
+}
